2981f79c4adaffd0190c01caa7aee92979f90b7c
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-11-27 22:40:29 +0800

    protein introduction

--- func/protein_func.R
+++ func/protein_func.R
@@ -4,12 +4,19 @@
 
 
 fn_panel_protein <- function(.choice){
+  tagList(
   column(
     width = 12, offset = 0,
     pickerInput(
       inputId = "input_protein_set", choices = .choice$protein, width = "800px", options = list( `live-search` = TRUE, size = 5, title = "Select interested protein symbol")
     )
-  )
+  ),
+  column(
+    width = 10,offset = 1,
+    shiny::tags$p(
+      "Protein level expression is quantified by reverse phase protein array (RPPA). It includes the cancer related ~200 protein and corresponding phosphorylated status."
+    )
+  ))
 }
 
 
