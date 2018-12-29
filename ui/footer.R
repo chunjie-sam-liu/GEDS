fc0e2016aab4e6644ffc30c79b6e2327b3d80312
jiikl7777 <344254557@qq.com>
2018-12-29 10:22:54 +0800

    footer

--- ui/footer.R
+++ ui/footer.R
@@ -10,9 +10,16 @@ fluidRow(
     ",",
     shiny::tags$a("HUST", href = "http://www.hust.edu.cn/", target = "_blank"),
     ", China", 
-    
     shiny::tags$br(),
-    
     "Any comments and suggestions, please contact us."
+  ),
+  shiny::tags$div(
+    class = "row",
+    shiny::tags$div(
+      class = "col-md-offset-3 col-md-3",
+      shiny::HTML(text = '<div style="display:inline-block;width:200px;">
+              <script type="text/javascript" src="//ra.revolvermaps.com/0/0/7.js?i=0a8z4i1jsww&amp;m=0&amp;c=ff0000&amp;cr1=ffffff&amp;sx=0" async="async"></script>
+                  </div>')
+    )
   )
 )
